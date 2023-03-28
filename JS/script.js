d7a37cb0c5deb842dc8230b8f685db3e67b46b0e
FrancescaLoria <francesca.loria90@hotmail.it>
2023-03-28 17:35:53 +0200

    completed

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: " La mia prima prova di Vue ",
+      url: "gandalf-ilsignoredeglianelli.webp",
+    };
+  },
+}).mount("#root");
